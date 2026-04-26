a2b84edb259a98f620f297afaa9ae1ab5b63b307
varshneymanav27 <varshneymanav27@gmail.com>
2026-04-26 12:36:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const gifStages = [
     "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
+    "https://media.tenor.com/fvhG4Kl8Q_kAAAAi/sad.gif",  // 1 confused
     "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
+    "https://media.tenor.com/8O3xAYxzp2UAAAAi/pengu-pudgy.gif",             // 3 sad
     "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
     "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
     "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
@@ -10,15 +10,15 @@ const gifStages = [
 ]
 
 const noMessages = [
-    "No",
-    "Are you positive? 🤔",
-    "Pookie please... 🥺",
-    "If you say no, I will be really sad...",
-    "I will be very sad... 😢",
+    "Noiiiiii",
+    "Are you sure? 🤔",
+    "Please naaaaaaaa... 🥺",
+    "If you say no, my smile might fade for a while",
+    "I’ll be kinda sad… not gonna lie... 😢",
     "Please??? 💔",
-    "Don't do this to me...",
+    "Hey, don’t do me like that...",
     "Last chance! 😭",
-    "You can't catch me anyway 😜"
+    "Catch me if you can! 😜"
 ]
 
 const yesTeasePokes = [
